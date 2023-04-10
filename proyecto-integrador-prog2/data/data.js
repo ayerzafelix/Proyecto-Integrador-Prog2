cd3aa6297130ffe3ad9697fe3847381805e9a02d
Santiroca <111546253+Santiroca@users.noreply.github.com>
2023-04-10 11:19:34 -0300

    css

--- proyecto-integrador-prog2/data/data.js
+++ proyecto-integrador-prog2/data/data.js
@@ -1,72 +1,100 @@
 const data = {
     productos: [
         {
-            nombre: 'Cafetera',
-            imgUrl: '/images/products/img-cafetera-moulinex.jpg', 
-            descripcion: 'Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex',
+            nombre: 'CBO',
+            imgUrl: '/images/products/Cbo.jpg', 
+            descripcion: 'Deliciosa hamburguesa con tus tres ingredientes favoritos: Chicken, Bacon, Onion. Delicioso pollo, crujiente bacon y el sabor sorprendente de nuestro crispy onion, con lechuga, queso cheddar, una salsa original y un pan único.',
             comments: [
                 {
-                    Comentarios: 12
+                    comentarios: 12
                 }
             ] 
 
 
         },
         {
-            nombre: 'Cafetera 2',
-            imgUrl: '/images/products/default-image.png', 
-            descripcion: 'Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex',
+            nombre: 'Big Mac',
+            imgUrl: '/images/products/bigMac.jpg', 
+            descripcion: 'Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.',
             comments: [
                 {
-                    Comentarios: 12
+                    comentarios: 12
                 }
             ] 
 
 
         },
         {
-            nombre: 'Cafetera 3',
-            imgUrl: '/images/products/default-image.png', 
-            descripcion: 'Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex',
+            nombre: 'Grand Tasty Doble',
+            imgUrl: '/images/products/grandTastyDoble.jpg', 
+            descripcion: 'Inigualable pan con semillas de sésamo, dos medallones de carne 100% vacuna, dos fetas de nuestro exclusivo Queso Cheddar, cebolla, lechuga y tomate frescos. Sumado a estos ingredientes la única e inigualable Salsa Tasty, un exclusivo sabor McDonald’s.',
             comments: [
                 {
-                    Comentarios: 12
+                    comentarios: 12
                 }
             ] 
 
 
         },
         {
-            nombre: 'Cafetera 4',
-            imgUrl: '/images/products/img-cafetera-moulinex.jpg', 
-            descripcion: 'Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex',
+            nombre: 'Hamburguesa Con Queso',
+            imgUrl: '/images/products/hamburguesaConQueso.jpg', 
+            descripcion: 'Carne 100% vacuna, queso derretido y mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar. Un clásico que nunca pasa de moda.',
             comments: [
                 {
-                    Comentarios: 12
+                    comentarios: 12
                 }
             ] 
 
 
         },
         {
-            nombre: 'Cafetera 5',
-            imgUrl: '/images/products/default-image.png', 
-            descripcion: 'Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex',
+            nombre: 'Club House',
+            imgUrl: '/images/products/clubHouse.jpg', 
+            descripcion: 'Incluye un nuevo pan artesanal tipo brioche, bacon rústico, cebolla grillada, queso cheddar suave, rodajas de tomate, lechuga y una salsa especial',
             comments: [
                 {
-                    Comentarios: 12
+                    comentarios: 12
                 }
             ] 
 
 
         },
         {
-            nombre: 'Cafetera 6',
-            imgUrl: '/images/products/default-image.png', 
-            descripcion: 'Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex',
+            nombre: 'Nuggets',
+            imgUrl: '/images/products/nuggets.jpg', 
+            descripcion: 'Hechos de carne blanca de pollo sin colorantes, sabores ni conservantes artificiales.',
             comments: [
                 {
-                    Comentarios: 12
+                    comentarios: 12
+                }
+            ] 
+
+
+        },
+
+
+        {
+            nombre: 'Mc Nifica',
+            imgUrl: '/images/products/mcnifica.jpg', 
+            descripcion: 'En un mundo donde todos buscan lo nuevo todo el tiempo, la McNífica viene a rectificar lo bueno de ser clásico. Carne, queso cheddar, tomate, lechuga y cebolla, acompañados de kétchup, mostaza y mayonesa.',
+            comments: [
+                {
+                    comentarios: 12
+                }
+            ] 
+
+
+        },
+
+
+        {
+            nombre: 'Mc Wrap',
+            imgUrl: '/images/products/Mcwrap.jpg', 
+            descripcion: 'Un crujiente pollo crispy, acompañado con cebolla, tomate, lechuga, mostaza y mayonesa.  Y por supuesto, todo envuelto en una masa suave y liviana.',
+            comments: [
+                {
+                    comentarios: 12
                 }
             ] 
 
